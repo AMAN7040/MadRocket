4268159eff6ec3c5c31df9782d842fe430829681
Aman Singh <amansingh79620@gmail.com>
2025-04-29 22:38:28 +0530

    fix(bug): fixed bug in deboucing functionality

--- src/context/SearchContext.jsx
+++ src/context/SearchContext.jsx
@@ -1,13 +1,16 @@
-// src/context/SearchContext.js
 import { createContext, useContext, useState } from "react";
+import useDebounce from "../hooks/useDebounce";
 
 const SearchContext = createContext();
 
 export const SearchProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState("");
+  const debounceSearchTerm = useDebounce(searchTerm, 300);
 
   return (
-    <SearchContext.Provider value={{ searchTerm, setSearchTerm }}>
+    <SearchContext.Provider
+      value={{ searchTerm, setSearchTerm, debounceSearchTerm }}
+    >
       {children}
     </SearchContext.Provider>
   );
