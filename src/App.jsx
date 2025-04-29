4c3c3b24156c557d513ae978425178899a392d5a
Aman Singh <amansingh79620@gmail.com>
2025-04-29 13:02:26 +0530

    chore: temporarily render raw pokemon list to verify API connectivity

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { usePokemon } from "./hooks/usePokemon";
+
 function App() {
+  const { data, isLoading, isError, error } = usePokemon();
+
+  if (isLoading) return <div>Loading....</div>;
+
+  if (isError) return <div>{error?.message}</div>;
+
   return (
     <>
-      <div className="text-red-300">WELCOME AMAN</div>
+      <div className="">
+        {data.map((pokemon) => (
+          <p>{pokemon.name}</p>
+        ))}
+      </div>
     </>
   );
 }
