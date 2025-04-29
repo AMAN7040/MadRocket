448d338aad37610d94b2e76698d3faa329c23287
Aman Singh <amansingh79620@gmail.com>
2025-04-29 18:26:57 +0530

    ui(skeleton): update styling

--- src/components/SkeletonCard.jsx
+++ src/components/SkeletonCard.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const SkeletonCard = () => (
-  <div className="bg-gray-100 animate-pulse rounded-md p-4 h-[150px]">
-    <div className="h-7 w-3/4 bg-gray-200 rounded mb-2" />
-    <div className="h-5 w-1/2 bg-gray-200 rounded" />
+  <div className="bg-gray-100 animate-pulse rounded-md p-4 h-[400px] w-[300px]">
+    <div className="h-1/3 w-3/4 bg-gray-200 rounded mb-2" />
+    <div className="h-2/3 w-full bg-gray-200 rounded" />
   </div>
 );
 
